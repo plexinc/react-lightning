71c62b58ae0b6ea4ccd9c6981ef9c1319a59e075
Andre Marques <marques.andre.lm@gmail.com>
2025-10-23 20:43:14 +0100

    remove _ for used prop

--- packages/plugin-reanimated/src/exports/withDelay.tsx
+++ packages/plugin-reanimated/src/exports/withDelay.tsx
@@ -9,11 +9,11 @@ export type WithDelayFn = (
 
 export const withDelay: WithDelayFn = (
   _delayMs,
-  _nextAnimation,
+  nextAnimation,
   _reduceMotion,
 ): AnimationObject => {
   // withTiming and withSpring supports `delay`. The client should pass it directly
-  return typeof _nextAnimation === 'function'
-    ? (_nextAnimation as () => AnimationObject)()
-    : (_nextAnimation as AnimationObject);
+  return typeof nextAnimation === 'function'
+    ? (nextAnimation as () => AnimationObject)()
+    : (nextAnimation as AnimationObject);
 };
